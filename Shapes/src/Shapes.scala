1dd7efe8a32d449b3271813dcbdf3d451504d7b3
maheshkkolla <mahesh.mk.245@gmail.com>
2015-06-01 11:23:52 +0530

    [Mahesh] Added square to shapes

--- Shapes/src/Shapes.scala
+++ Shapes/src/Shapes.scala
@@ -1,4 +1,3 @@
-import java.awt.Rectangle
 
 /**
  * Created by mahesh on 01/06/15.
@@ -7,5 +6,7 @@ object Shapes {
   def main (args: Array[String]) {
     val r1 = new Rectangle(5, 5);
     println(r1.toString() +"\tArea: "+r1.calculateArea() +", Perimeter: "+r1.calculatePerimeter())
+    val s1 = new Square(2);
+    println(s1);
   }
 }
---
+++ Shapes/src/Square.scala
@@ -0,0 +1,7 @@
+/**
+ * Created by mahesh on 01/06/15.
+ */
+class Square(s :Int) {
+  var side :Int = s
+  override def toString(): String = "Square(Side: "+side+")"
+}
